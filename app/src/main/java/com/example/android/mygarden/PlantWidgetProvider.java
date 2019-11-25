8724ffafd7d6296b4a3f32675ef72c1b72813ce0
asengsaragih <aldiwahyu.saragih@gmail.com>
2019-11-25 13:28:24 +0700

    plant widget provider

---
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -0,0 +1,29 @@
+package com.example.android.mygarden;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.android.mygarden.ui.MainActivity;
+
+public class PlantWidgetProvider extends AppWidgetProvider {
+
+    static void updateWidget(Context context, AppWidgetManager manager, int id) {
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+
+        remoteViews.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+        manager.updateAppWidget(id, remoteViews);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager manager, int[] ids) {
+        for (int id : ids) {
+            updateWidget(context, manager, id);
+        }
+    }
+}
