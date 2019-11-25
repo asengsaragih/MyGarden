a5e47d4241ec7c11b4f8451d7f6b287dbf1741db
asengsaragih <aldiwahyu.saragih@gmail.com>
2019-11-25 14:11:23 +0700

    Plant widget, PlantWateringService, PlantWidgetProvider

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -9,14 +9,20 @@ import android.widget.RemoteViews;
 
 import com.example.android.mygarden.ui.MainActivity;
 
+import static com.example.android.mygarden.PlantWateringService.ACTION_WATER_PLANTS;
+
 public class PlantWidgetProvider extends AppWidgetProvider {
 
     static void updateWidget(Context context, AppWidgetManager manager, int id) {
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
-
         remoteViews.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+
+        Intent i = new Intent(context, PlantWateringService.class);
+        i.setAction(ACTION_WATER_PLANTS);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.widget_water_button, pi);
         manager.updateAppWidget(id, remoteViews);
     }
 
